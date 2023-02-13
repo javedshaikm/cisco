667312451ce07e10732b7d19c470915cec47789c
javedshaikm <javed.s.m@hotmail.com>
2023-02-13 10:42:18 +0530

    Update ping-ips.py
    This version opens both files at the same time using a context manager (with statement) and uses f-strings for string formatting.

--- ping-ips.py
+++ ping-ips.py
@@ -1,25 +1,15 @@
-ip_list = open("D:\\python\\sdc-ip.txt").read().splitlines()
+import subprocess
 
-ip_list = list(set(ip_list))
-import os
-i=1
-j=1
-reach=[]
-not_reach=[]
-#for n, ip_address in enumerate(ip_list):
-for ip_address in ip_list:
-    response = os.system("ping -n 2 " + ip_address)
+ip_list = list(set(open("D:\\python\\sdc-ip.txt").read().splitlines()))
+reach = []
+not_reach = []
+for i, ip_address in enumerate(ip_list):
+    response = subprocess.call(['ping', '-n', '2', ip_address])
     if response == 0:
-        reach.append('{}. {} is reachable\n'.format(i,ip_address))
-        i+=1
+        reach.append(f"{i + 1}. {ip_address} is reachable\n")
     else:
-        not_reach.append('{}. {} is not reachable\n'.format(j,ip_address))
-        j+=1
+        not_reach.append(f"{i + 1}. {ip_address} is not reachable\n")
 
-with open('D:\\python\\ping_reachable.txt', 'a') as f:
-    for i in reach:
-        f.write(i)
-
-with open('D:\\python\\ping_unreachable.txt', 'a') as  f:
-    for i in not_reach:
-        f.write(i)
+with open('D:\\python\\ping_reachable.txt', 'a') as f_reach, open('D:\\python\\ping_unreachable.txt', 'a') as f_not_reach:
+    f_reach.write(''.join(reach))
+    f_not_reach.write(''.join(not_reach))
