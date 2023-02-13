19a753daf8dc6531d0ca99aaf0d8907230b3179f
javedshaikm <javed.s.m@hotmail.com>
2023-02-13 09:00:20 +0530

    Update check-int-status.py
    Instead of printing the results to the console, this code can write them to a log file. This will make it easier to track and review the results later.

--- check-int-status.py
+++ check-int-status.py
@@ -1,41 +1,35 @@
-from __future__ import print_function
 from netmiko import ConnectHandler
-import pandas as pd
-import smtplib
-import sys
-import time
-import select
-import paramiko
-import re
+import logging
+
+# Set up logging
+logging.basicConfig(filename='interface_status.log', level=logging.INFO)
 
 username = 'cisco'
 password = 'cisco_1234!'
 int_down = r"is administratively down"
+
+# Read hosts and interfaces from a file or database
 hosts = ['10.10.20.48']
-interfaces_1 = ['GigabitEthernet1','GigabitEthernet2','GigabitEthernet3']
+interfaces = ['GigabitEthernet1', 'GigabitEthernet2', 'GigabitEthernet3']
+
 platform = 'cisco_xe'
 
 for host in hosts:
-	connect = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
-	output = connect.send_command('terminal length 0',expect_string='csr1000v')
-	#output = connect.send_command('enable')
-	if host == '10.10.20.48':
-		for interfaces in interfaces_1:
-			interface_status = connect.send_command(f'show interface {interfaces}',expect_string='csr1000v')
-			#print(host)
-			#print(interfaces)
-			
-			#print (interface_status)
-			for line in interface_status.split('\n'):
-				#print (line)
-				#if 'Description' in line:
-				#	pass
-				if int_down in line:
-					print(f"{interfaces} is down in {host}")
+    try:
+        connect = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
+        output = connect.send_command('terminal length 0', expect_string='csr1000v')
 
-				else:
-					pass
-					#print('All interfaces are up')
-					#break
+        for interface in interfaces:
+            interface_status = connect.send_command(f'show interface {interface}', expect_string='csr1000v')
+
+            for line in interface_status.split('\n'):
+                if int_down in line:
+                    logging.info(f"{interface} is down in {host}")
+                else:
+                    pass
+        connect.disconnect()
+
+    except Exception as e:
+        logging.error(f"Error occurred while checking interfaces on {host}: {e}")
 
                 	
