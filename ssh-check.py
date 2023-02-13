93857c827b2e55bd7e7c3ffdddf79b32b0d7770c
javedshaikm <javed.s.m@hotmail.com>
2023-02-13 10:47:46 +0530

    Update ssh-check.py
    Optimized the code by using a context manager with the SSHClient object, so that the SSHClient object is automatically closed after each iteration of the loop.

--- ssh-check.py
+++ ssh-check.py
@@ -1,18 +1,18 @@
 import paramiko
 import socket
-dssh = paramiko.SSHClient()
-dssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ips = [i.strip() for i in open("C:\Python27\Testing\Fetch.txt")] # creates a list from input file
+
+ips = [i.strip() for i in open("C:\Python27\Testing\Fetch.txt")]
 
 for ip in ips:
-    try:
-        dssh.connect(ip, username='cisco', password='cisco', timeout=4)
-        stdin, stdout, stderr = ssh.exec_command('sh ip ssh')
-        print ip + '===' + stdout.read()
-        ssh.close()
-    except paramiko.AuthenticationException:
-        print ip + '=== Bad credentials'
-    except paramiko.SSHException:
-        print ip + '=== Issues with ssh service'
-    except socket.error:
-        print ip + '=== Device unreachable' 
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh.connect(ip, username='cisco', password='cisco', timeout=4)
+            stdin, stdout, stderr = ssh.exec_command('sh ip ssh')
+            print ip + '===' + stdout.read()
+        except paramiko.AuthenticationException:
+            print ip + '=== Bad credentials'
+        except paramiko.SSHException:
+            print ip + '=== Issues with ssh service'
+        except socket.error:
+            print ip + '=== Device unreachable'
