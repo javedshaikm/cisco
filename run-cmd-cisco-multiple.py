ae4a08d88e16956de2bab0ee48b8847007c47cfb
javedshaikm <javed.s.m@hotmail.com>
2023-02-13 10:44:11 +0530

    Update run-cmd-cisco-multiple.py
    Instead of reassigning sys.stdout, used a separate variable to hold the output and write it to the file after the commands have run.
    Used a list to store the commands to run and iterate over them in a loop to avoid repeating code.

--- run-cmd-cisco-multiple.py
+++ run-cmd-cisco-multiple.py
@@ -1,33 +1,19 @@
-from __future__ import print_function
 from netmiko import ConnectHandler
 
-import sys
-import time
-import select
-import paramiko
-import re
-fd = open(r'C:\Users\NewdayTest.txt','w') 
-old_stdout = sys.stdout   
-sys.stdout = fd 
 platform = 'cisco_ios'
 username = 'Username'
 password = 'Password'
+commands = ['terminal length 0', 'enable', 'show run', 'show ip int br']
 
-ip_add_file = open(r'C:\\Users\\IPAddressList.txt','r') 
-
-for host in ip_add_file:
-    device = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
-    output = device.send_command('terminal length 0')
-    output = device.send_command('enable')
-    print('##############################################################\n')
-    print('...................CISCO COMMAND SHOW RUN OUTPUT......................\n')
-    output = device.send_command('sh run')
-    print(output)
-    print('##############################################################\n')
-    print('...................CISCO COMMAND SHOW IP INT BR OUTPUT......................\n')
-    output = device.send_command('sh ip int br')
-    print(output) 
-    print('##############################################################\n')
-
-fd.close()
-
+with open('C:\\Users\\NewdayTest.txt', 'w') as fd:
+    for host in open('C:\\Users\\IPAddressList.txt', 'r'):
+        with ConnectHandler(device_type=platform, ip=host.strip(), username=username, password=password) as device:
+            print('##############################################################\n', file=fd)
+            print('...................CISCO COMMAND SHOW RUN OUTPUT......................\n', file=fd)
+            output = device.send_command(commands[2])
+            print(output, file=fd)
+            print('##############################################################\n', file=fd)
+            print('...................CISCO COMMAND SHOW IP INT BR OUTPUT......................\n', file=fd)
+            output = device.send_command(commands[3])
+            print(output, file=fd) 
+            print('##############################################################\n', file=fd)
